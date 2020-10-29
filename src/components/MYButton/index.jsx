28f7f66a8f2069b217915e1a2797aa83f3bf7c7f
pengchengming <1020196987@qq.com>
2020-10-29 23:28:09 +0800

    添加单元测试

--- src/components/MYButton/index.jsx
+++ src/components/MYButton/index.jsx
@@ -1,13 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class MYButton extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+  handleClick = () => {
+    console.log("click");
+  };
+  handleSelectChange = (e) => {
+    console.log(e.target);
+  };
+  render() {
+    let fn = (element) => {
+      console.log(element)
     }
-    render() {
-        return(
-            <div>button</div>
-        )
-    }
+    return (
+      <>
+      {
+        [1, 2, 3, 4].map((item ,index) => (
+          <input type="checkbox" value={item} ref={fn}/>
+        ))
+      }
+        
+        {/* <div>button</div>
+        <p onClick={this.handleClick}>点击</p>
+
+        <select name="" id="" onChange={(e) => this.handleSelectChange(e)}>
+          <option value="">aa</option>
+          <option value="">bb</option>
+          <option value="">cc</option>
+        </select> */}
+      </>
+    );
+  }
 }
 export default MYButton;
