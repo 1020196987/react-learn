6e769a31163e48d8ec01388a2dfeec0ea9e6511c
pengchengming <1020196987@qq.com>
2020-11-15 15:40:12 +0800

    2020年11月15日

---
+++ test/无处不在的函数.js
@@ -0,0 +1,16 @@
+'use strict'
+function Stack() {
+    const list = [];
+    // return {
+    //     push: function(x){
+    //         list.push(x);
+    //         console.log(list)
+    //     }
+    // }
+    console.log('aa')
+    console.log(this.setTimeout)
+}
+
+let s = Stack();
+// s.push(12);
+// console.log(s);
